357b76f63d10461e71f5624f5629134c400ff320
p4xt3r <121817930+algrvvv@users.noreply.github.com>
2024-08-21 14:45:09 +0300

    полностью переделан основной файл

--- cmd/monlog/main.go
+++ cmd/monlog/main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"github.com/algrvvv/monlog/internal/app"
+	"github.com/algrvvv/monlog/internal"
 	"github.com/algrvvv/monlog/internal/config"
 	"github.com/algrvvv/monlog/internal/logger"
-	"github.com/mdobak/go-xerrors"
-	"golang.org/x/crypto/ssh"
-	"io"
+	"github.com/algrvvv/monlog/internal/server"
 	"log"
-	"log/slog"
-	"strconv"
 )
 
 func main() {
@@ -25,73 +20,15 @@ func main() {
 		log.Fatal(err)
 	}
 
-	for _, s := range config.Cfg.Servers {
-		newSession(s)
-	}
-}
-
-func newSession(s config.ServerConfig) {
-	clientConfig, err := app.NewSSHConfig(config.Cfg.App.PathToIDRSA, s.User)
-	if err != nil {
-		logger.Logger.Error(err.Error(), slog.Any("error", xerrors.New(err)))
-	}
-
-	logger.Logger.Info("Connection to server...")
-	var client *ssh.Client
-	addr := fmt.Sprintf("%s:%d", s.Host, s.Port)
-	client, err = ssh.Dial("tcp", addr, clientConfig)
-	if err != nil {
-		logger.Logger.Error(err.Error(), slog.Any("error", xerrors.New(err)))
-	}
-	defer client.Close()
-
-	logger.Logger.Info("Connected to server", slog.Any("server", addr))
-
-	session, err := client.NewSession()
-	if err != nil {
-		logger.Logger.Error(err.Error(), slog.Any("error", xerrors.New(err)))
-	}
-	defer session.Close()
-	logger.Logger.Info("Created new session to server", slog.Any("server", addr))
-
-	var startLine string
-	if s.StartLine == "0" {
-		startLine = "1"
-	} else {
-		startLine = s.StartLine
-	}
-	cmd := fmt.Sprintf("tail -n +%s %s && tail -f %s", startLine, s.LogDir, s.LogDir)
-	reader, writer := io.Pipe()
-	defer reader.Close()
-
-	session.Stdout = writer
-	session.Stderr = writer
-
-	err = session.Start(cmd)
-	if err != nil {
-		logger.Logger.Error(err.Error(), slog.Any("error", xerrors.New(err)))
-	}
-
 	go func() {
-		scanner := bufio.NewScanner(reader)
-		currentLine, _ := strconv.Atoi(startLine)
-		for scanner.Scan() {
-			line := scanner.Text()
-			processLogLine(line, currentLine)
-			currentLine++
-		}
-
-		if err = scanner.Err(); err != nil && err != io.EOF {
-			log.Fatalf("Error reading output: %v", err)
+		for i, s := range config.Cfg.Servers {
+			go internal.ConnectAndReadLogs(i, s)
 		}
 	}()
 
-	err = session.Wait()
-	if err != nil && err != io.EOF {
-		logger.Logger.Error(err.Error(), slog.Any("error", xerrors.New(err)))
+	serv := server.NewServer()
+	logger.Info(fmt.Sprintf("Starting server on :%d", config.Cfg.App.Port))
+	if err = serv.ListenAndServe(); err != nil {
+		logger.Error(err.Error(), err)
 	}
-}
-
-func processLogLine(line string, currentLine int) {
-	fmt.Printf("COPY FROM SERVER: [%d] - %s\n", currentLine, line)
 }
