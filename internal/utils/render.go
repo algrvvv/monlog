12af730d65a5008477d0ef705d96dba3540e27a9
p4xt3r <121817930+algrvvv@users.noreply.github.com>
2024-08-21 14:39:49 +0300

    добавлена функция для редиректа на страницу ошибки

---
+++ internal/utils/render.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"github.com/algrvvv/monlog/internal/logger"
+	"html/template"
+	"net/http"
+)
+
+type templateError struct {
+	Error      string // сообщение об ошибке
+	StatusCode int    // статус ответа
+	StatusText string // текстовый статус ответа
+}
+
+// RenderError TODO - исправить
+func RenderError(w http.ResponseWriter, message string, statusCode int) {
+	temp, err := template.New("error").ParseFiles("templates/error.html")
+	if err != nil {
+		logger.Error(err.Error(), err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(statusCode)
+	te := templateError{
+		message, statusCode, http.StatusText(statusCode),
+	}
+	err = temp.Execute(w, te)
+	if err != nil {
+		logger.Error(err.Error(), err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
