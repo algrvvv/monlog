69da4d3fba646979ccda58ae78d67e08d396f15a
p4xt3r <121817930+algrvvv@users.noreply.github.com>
2024-08-28 22:47:16 +0300

    добавлена валидация некоторых полей конфигурации

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,30 +2,33 @@ package config
 
 import (
 	"errors"
+	"fmt"
+	"log"
 	"os"
 
+	"github.com/go-playground/validator/v10"
 	"gopkg.in/yaml.v3"
 )
 
 type AppConfig struct {
 	TGBotToken        string `yaml:"tg_bot_token"`
-	PathToIDRSA       string `yaml:"path_to_id_rsa"`
-	Port              int    `yaml:"port"`
-	MaxLocalLogSizeMB int    `yaml:"max_local_log_size_mb"`
-	NumberRowsToLoad  int    `yaml:"number_rows_to_load"`
+	PathToIDRSA       string `yaml:"path_to_id_rsa" validate:"required"`
+	Port              int    `yaml:"port" validate:"required"`
+	MaxLocalLogSizeMB int    `yaml:"max_local_log_size_mb" validate:"required"`
+	NumberRowsToLoad  int    `yaml:"number_rows_to_load" validate:"required"`
 }
 
 type ServerConfig struct {
 	Enabled   bool     `yaml:"enabled"`
-	Name      string   `yaml:"name"`
-	Host      string   `yaml:"host"`
-	User      string   `yaml:"user"`
-	Port      int      `yaml:"port"`
-	LogDir    string   `yaml:"log_dir"`
-	StartLine string   `yaml:"start_line"`
+	Name      string   `yaml:"name" validate:"required"`
+	Host      string   `yaml:"host" validate:"-"`
+	User      string   `yaml:"user" validate:"-"`
+	Port      int      `yaml:"port" validate:"-"`
+	LogDir    string   `yaml:"log_dir" validate:"required"`
+	StartLine string   `yaml:"start_line" validate:"required"`
 	ChatIDs   []string `yaml:"chat_ids"`
 	Notify    bool     `yaml:"notify"`
-	IsLocal   bool     `yaml:"is_local"`
+	IsLocal   bool     `yaml:"is_local" validate:"checkHUP"`
 }
 
 type Keywords struct {
@@ -42,6 +45,46 @@ type Config struct {
 	Keywords Keywords       `yaml:"keywords"`
 }
 
+func checkHUPValidator(fl validator.FieldLevel) bool {
+	config := fl.Parent().Interface().(ServerConfig)
+	if !config.IsLocal {
+		if config.Host == "" || config.Port == 0 || config.User == "" {
+			return false
+		}
+	}
+	return true
+}
+
+func translateError(err validator.ValidationErrors) map[string]string {
+	customErrMsg := make(map[string]string)
+	getErrorMsg := func(fieldError validator.FieldError) string {
+		if fieldError.Tag() == "required" {
+			return fmt.Sprintf("%s is required field [%s(%s)]", fieldError.StructField(), fieldError.Namespace(), fieldError.Kind())
+		} else if fieldError.Tag() == "checkHUP" {
+			return fmt.Sprintf("If server is not local, his host, user and port required [%s]", fieldError.Namespace())
+		}
+		return fieldError.Error()
+	}
+
+	for _, fieldError := range err {
+		customErrMsg[fieldError.StructField()] = getErrorMsg(fieldError)
+	}
+	return customErrMsg
+}
+
+func validateConfigPart(validate *validator.Validate, s interface{}) error {
+	if err := validate.Struct(s); err != nil {
+		var validationErrors validator.ValidationErrors
+		errors.As(err, &validationErrors)
+		customErrMsg := translateError(validationErrors)
+		for field, message := range customErrMsg {
+			log.Printf("Config validation error: %s: %s\n", field, message)
+		}
+		return errors.New("failed to parse config.yml")
+	}
+	return nil
+}
+
 var Cfg Config
 
 func LoadConfig(filepath string) error {
@@ -53,6 +96,18 @@ func LoadConfig(filepath string) error {
 	var config Config
 	if err = yaml.Unmarshal(data, &config); err != nil {
 		return errors.New("failed to parse config.yaml" + err.Error())
+	}
+
+	validate := validator.New()
+	_ = validate.RegisterValidation("checkHUP", checkHUPValidator)
+
+	if err = validateConfigPart(validate, config.App); err != nil {
+		log.Fatal(err)
+	}
+	for i, server := range config.Servers {
+		if err = validateConfigPart(validate, server); err != nil {
+			log.Fatalf("[server-%d] %v", i, err)
+		}
 	}
 
 	Cfg = config
