a9038c7b086a8df16e831b2aa22279e4eb03f058
p4xt3r <121817930+algrvvv@users.noreply.github.com>
2024-11-29 14:18:09 +0300

    добавлены функции для регистрации и использования нужного драйвера

---
+++ internal/drivers/registry/registry_handle.go
@@ -0,0 +1,35 @@
+package drivers
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/algrvvv/monlog/internal/types"
+)
+
+type DriverFactory func() types.LineHandleDriver
+
+var drivers = make(map[string]types.LineHandleDriver)
+
+func RegisterDriver(name string, factory DriverFactory) {
+	if _, ok := drivers[name]; ok {
+		panic("Driver factory already registered: " + name)
+	}
+	drivers[name] = factory()
+}
+
+func Handle(driverName, line string) string {
+	if driverName == "" {
+		return line
+	}
+
+	// driverName = strings.TrimPrefix(driverName, "use drivers;")
+	driverName = strings.TrimSpace(driverName)
+
+	if driver, ok := drivers[driverName]; !ok {
+		fmt.Printf("WARNING! driver with name %s not found\n", driverName)
+		return line
+	} else {
+		return driver.Handle(line)
+	}
+}
